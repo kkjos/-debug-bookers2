efb34b2a6e3d9167b73930c9f837709fda98e491
“kkjos” <k19951227@gmail.com>
2021-10-25 15:22:10 +0000

    [add]フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+      resource :relationships, only: [:index, :create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
 
   resources :books, only: [:new, :create, :index, :show, :update, :edit, :destroy] do
     resource :favorites, only: [:create, :destroy]
