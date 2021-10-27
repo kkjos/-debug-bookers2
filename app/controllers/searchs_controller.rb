6c83721e2252e89861e0dd0bfff2bdd19ffa980e
“kkjos” <k19951227@gmail.com>
2021-10-27 12:57:05 +0000

    [finish]応用課題５/add一致条件

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,24 +2,32 @@ class SearchsController < ApplicationController
   def search
     @model = params["model"]
     @content = params["content"]
-    @method = params["content"]
+    @method = params["method"]
     @records = search_for(@model, @content, @method)
   end
 
   private
   def search_for(model, content, method)
     if model == 'user'
+
       if method == 'perfect'
         User.where(name: content)
       else
         User.where('name Like ?', '%'+content+'%')
       end
+
     elsif model == 'book'
+
       if method == 'perfect'
         Book.where(title: content)
-      else
+      elsif method == 'forward'
+        Book.where('title Like ?', content+'%')
+      elsif method == 'backward'
+        Book.where('title like ?', '%'+content)
+      else method == 'partial'
         Book.where('title Like ?', '%'+content+'%')
       end
+
     end
   end
 end
